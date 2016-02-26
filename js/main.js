d6ff922e40c589dcd6fa0d6caf985c2e35730990
Lenzo <lenbunce@Lens-MacBook-Pro.local>
2016-02-26 13:10:38 -0500

    set vars

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var begin = alert('Let’s make a story together!');
+
+var colour = prompt('A colour (orange, indigo, etc.)');
+var creature = prompt('A creature (dragon, moth, etc.)');
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n Enter the letter below:');
+
